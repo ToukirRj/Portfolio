432694ef9c68b7c8ed0c79945f47517af0e1b97f
Toukir Rahman <toukir@nextit.com.bd>
2024-03-03 17:16:16 +0600

    no message

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import Footer from './Footer'
 import Animation from '../props/Animation'
 import SmoothScroll from '../props/SmoothScroll'
+import CursorAnimation from '../props/CursorAnimation'
 
 function MainLayout({children}){
     window. scrollTo(0, 0)
@@ -13,6 +14,7 @@ function MainLayout({children}){
             <Footer/>
             <Animation/>
             <SmoothScroll/>
+            <CursorAnimation/>
         </>
     )
 }
