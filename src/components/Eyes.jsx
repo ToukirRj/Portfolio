6177abc7784355aebd54b4f42db7b396981cd2bf
DESKTOP-6P64BKE\Toukir Rahman <to.toukir.rahman@gmail.com>
2024-03-26 22:25:58 +0600

    no message

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -14,24 +14,23 @@ const Eyes = () => {
                         </p>
                     </div>
                     <div className="sm:mt-12 mt-8">
-                        <div className="gap-6 space-y-0 flex sm:flex-row flex-col items-center">
-                            <div className="relative w-1/3 divide-y divide-[#1F2937]/[0.2] animatable">
-                                {serviceData.map(service => (
-                                    <div key={service.id} className="flex flex-col items-center gap-3 py-6 animatable">
-                                        <div className="w-1/12">
-                                            <span className='w-16 h-16 rounded-xl bg-white/[0.85] shadow-lg shadow-primary/10 flex items-center justify-center'>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke={service.iconColor} strokeWidth=".85" strokeLinecap="round" strokeLinejoin="round" className={`feather feather-${service.iconPath}`} dangerouslySetInnerHTML={{ __html: service.iconStyle }}/>
-                                            </span>
-                                        </div>
-                                        <div className='w-3/12'>
-                                            <h4 className="text-[33px] leading-[36px] font-[700] tracking-tight text-[#1F2937] sm:text-start text-center">{service.title}</h4>
-                                        </div>
-                                        <div className='w-8/12'>
-                                            <p className="mt-1 text-[15px] font-[400] text-[#1F2937] tracking-wide sm:text-start text-center">{service.description}</p>
-                                        </div>
+                        <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-3 gap-3 items-center">
+                            {serviceData.map(service => (
+                                <div key={service.id} className="relative group overflow-hidden p-[32px] rounded-[24px] border border-[#27272a]">
+                                    <div className="inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-[#111] to-white blur-2xl opacity-25 group-hover:opacity-30"></div>
+                                    <div className="w-1/12">
+                                        <span className='w-16 h-16 rounded-xl bg-white/[0.85] shadow-lg shadow-primary/10 flex items-center justify-center'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke={service.iconColor} strokeWidth=".85" strokeLinecap="round" strokeLinejoin="round" className={`feather feather-${service.iconPath}`} dangerouslySetInnerHTML={{ __html: service.iconStyle }}/>
+                                        </span>
                                     </div>
-                                ))}
-                            </div>
+                                    <div className='w-3/12'>
+                                        <h4 className="text-[31px] leading-[36px] font-[700] tracking-tight text-[#1F2937] sm:text-start text-center">{service.title}</h4>
+                                    </div>
+                                    <div className='w-8/12'>
+                                        <p className="mt-1 text-[15px] font-[400] text-[#1F2937] tracking-wide sm:text-start text-center">{service.description}</p>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
