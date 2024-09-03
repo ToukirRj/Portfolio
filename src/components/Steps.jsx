d49676cfd9cb5bcf3a68e0de774e52ad1fbfafdb
Toukir\Toukir Rahman <to.toukir.rahman@gmail.com>
2024-09-04 01:04:32 +0600

    no message

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -21,12 +21,17 @@ const Steps = () => {
                                 <img src={Step} alt="steps"/>
                             </div>
                         </div>
-                        <div className="w-full">
+                        <div className="w-full ps-10">
                             {stepsData.map(step => (
-                                <div key={step.id} className="relative w-full mb-5 p-7 backdrop-blur-lg bg-white/[.25] shadow-[0_3px_35px_0px_rgba(2,24,93,0.095)] rounded-[30px]">
-                                    <div className="bg-[#1F2937] w-0.5 h-full top-4 left-[13px] absolute"></div>
-                                    <h2 className='px-4 h-[30px] flex items-center text-[12px] font-[600] text-[#1F2937] tracking-wider uppercase bg-white/[.75] rounded-[30px]'>Step Name</h2>
-                                    <h4 className="font-[600] text-[17px] tracking-normal text-gray-700">{step.Name}</h4>
+                                <div key={step.id} className="relative w-full">
+                                    <span className='absolute -top-[12px] -left-16 w-[80px] h-[80px] rounded-full bg-[#1F2937] flex items-center justify-center'>
+                                        <h3 className='text-[30px] font-[600] text-white'>{step.id}</h3>
+                                    </span>
+                                    <div className="relative w-full mb-10 px-7 py-4 backdrop-blur-lg bg-white/[.15] shadow-[0_3px_35px_0px_rgba(2,24,93,0.095)] rounded-[20px]">
+                                        <div className="bg-[#1F2937] w-0.5 h-full top-4 left-[13px] absolute"></div>
+                                        {/* <h2 className='px-4 h-[30px] flex items-center text-[12px] font-[600] text-[#1F2937] tracking-wider uppercase bg-white/[.75] rounded-[30px]'>Step Name</h2> */}
+                                        <h4 className="font-[600] text-[17px] tracking-normal text-gray-700">{step.Name}</h4>
+                                    </div>
                                 </div>
                             ))}
                         </div>
