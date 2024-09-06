aaf241472d52fe0d4dfa1831d0291de5e6370759
Toukir\Toukir Rahman <to.toukir.rahman@gmail.com>
2024-09-07 03:04:47 +0600

    no message

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -13,7 +13,7 @@ const Steps = () => {
                                 <h2 className="text-[67px] font-bold text-[#1F2937] leading-[74px]"> 
                                     <span className="bg-gradient-to-r from-[#ff8144] to-[#17b057] inline-block text-transparent bg-clip-text ps-3">Steps</span> of <br/>A Project
                                 </h2>
-                                <p className="mx-auto max-w-full mt-2 text-[#1F2937] w-full text-[21px] font-[300] leading-[37px]">
+                                <p className="mx-auto max-w-full mt-2 text-[#1F2937] w-full text-[19px] font-[300] leading-[37px] tracking-wide">
                                     I use these steps to reach the destination of your project needs
                                 </p>
                             </div>
@@ -23,15 +23,17 @@ const Steps = () => {
                         </div>
                         <div className="w-full ps-10">
                             {stepsData.map(step => (
-                                <div key={step.id} className="relative w-full flex items-center mb-5 px-7 py-2 backdrop-blur-lg bg-white/[.15] shadow-[0_3px_35px_0px_rgba(2,24,93,0.095)] rounded-[45px]">
-                                    <span className='w-[40px] h-[40px] rounded-full bg-[#1F2937] flex items-center justify-center'>
-                                        <h3 className='text-[23px] font-[600] text-white'>{step.id}</h3>
+                                <div key={step.id} className="step relative z-10 w-full mb-5">
+                                    <span className='absolute -z-50 -top-[17px] -left-[31px] w-[40px] h-[40px] rounded-l-[40px] rounded-tr-[40px] bg-[#1F2937] flex items-center justify-center'>
+                                        <h3 className='text-[20px] font-[600] text-white'>{step.id}</h3>
                                     </span>
-                                    <div>
-                                        <h2 className='text-[12px] font-[600] text-[#1F2937] tracking-wider uppercase bg-white/[.75] rounded-[30px] table'>{step.Name}</h2>
-                                        <h4 className="text-[17px] font-[600] text-[#1F2937] tracking-normal">{step.Name}</h4>
+                                    <div className="px-5 py-4 backdrop-blur-lg bg-white/[.085] shadow-[0_3px_35px_0px_rgba(2,24,93,0.15)] rounded-[25px]">
+                                        <div>
+                                            <div className="px-4 py-[3px] w-auto rounded-[30px] bg-[#FF8045]/[.28] text-[13px] font-[500] text-[#1F2937] tracking-wide table">{step.topic}</div>
+                                            <h4 className="mt-2 text-[20px] font-[300] text-[#1F2937] tracking-normal">{step.Name}</h4>
+                                        </div>
+                                        <span className="line bg-[#1F2937] w-0.5 h-full top-4 -left-[11px] absolute"></span>
                                     </div>
-                                    <span className="bg-[#1F2937] w-0.5 h-full top-4 left-[13px] absolute"></span>
                                 </div>
                             ))}
                         </div>
--- src/datas/stepsData.jsx
+++ src/datas/stepsData.jsx
@@ -3,39 +3,33 @@
 const stepsData = [
     { 
         id: '1',
-        iconNumber: "0 0 15 15",
-        Name: 'Understand The Demands',
-        iconStyle: `<path fill="white" d="M7.5 12L0 4h15l-7.5 8Z"/>`
+        topic: 'Requirements',
+        Name: 'Understand The Demands'
     },
     { 
         id: '2',
-        iconNumber: "0 0 24 24",
-        Name: 'Pick Related Research',
-        iconStyle: `<path fill="none" stroke="white" stroke-width="2" d="m2 8.35l10.173 9.823L21.997 8"/>`
+        topic: 'Analysis',
+        Name: 'Pick Related Research'
     },
     { 
         id: '3',
-        iconNumber: "0 0 24 24",
-        Name: 'Analyze User Experiences',
-        iconStyle: `<path fill="none" stroke="white" stroke-width="2" d="m2 8.35l10.173 9.823L21.997 8"/>`
+        topic: 'UX Frame',
+        Name: 'Analyze User Experiences'
     },
     { 
         id: '4',
-        iconNumber: "0 0 24 24",
-        Name: 'Prepare the First-Look', 
-        iconStyle: `<path fill="none" stroke="white" stroke-width="2" d="m2 8.35l10.173 9.823L21.997 8"/>`
+        topic: 'UI Implement',
+        Name: 'Prepare the First-Look'
     },
     { 
         id: '5',
-        iconNumber: "0 0 24 24",
-        Name: 'Arranged with Revision', 
-        iconStyle: `<path fill="none" stroke="white" stroke-width="2" d="m2 8.35l10.173 9.823L21.997 8"/>`
+        topic: 'Feedback',
+        Name: 'Arranged with Revision'
     },
     { 
         id: '6',
-        iconNumber: "0 0 256 256",
-        Name: 'Build Final Version', 
-        iconStyle: `<path fill="white" d="m232.49 80.49l-128 128a12 12 0 0 1-17 0l-56-56a12 12 0 1 1 17-17L96 183L215.51 63.51a12 12 0 0 1 17 17Z"/>`
+        topic: 'Finalization',
+        Name: 'Build Final Version'
     },
 ];
 
