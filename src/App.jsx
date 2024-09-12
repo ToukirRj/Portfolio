3fb5566cf2c2872bf53f54b7baffcb4cd3b20616
Toukir Rahman <toukir@nextit.com.bd>
2024-09-12 16:43:56 +0600

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-
-import Home from './pages/Home'
+import Home from './pages/Home';
+// import { useEffect } from 'react';
 
 function App() {
+  // useEffect(() => {
+  //   const disableContextMenu = (e) => {
+  //     e.preventDefault();
+  //   };
+
+  //   const handleKeyDown = (e) => {
+  //     // Disable F12, Ctrl+Shift+I (DevTools), Ctrl+Shift+C (Inspect)
+  //     if (
+  //       e.key === 'F12' || 
+  //       (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'C'))
+  //     ) {
+  //       e.preventDefault();
+  //     }
+  //   };
+
+  //   // Attach event listeners
+  //   window.addEventListener('contextmenu', disableContextMenu);
+  //   window.addEventListener('keydown', handleKeyDown);
+
+  //   // Cleanup event listeners on unmount
+  //   return () => {
+  //     window.removeEventListener('contextmenu', disableContextMenu);
+  //     window.removeEventListener('keydown', handleKeyDown);
+  //   };
+  // }, []);
+
   return (
     <>
-        <Home/>
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
